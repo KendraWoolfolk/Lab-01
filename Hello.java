677dd272d14c51c7b985b6b2b3f7c8aa2bfe68b0
KendraWoolfolk <98036553+KendraWoolfolk@users.noreply.github.com>
2022-01-19 09:58:52 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+/********************************************************************************************* Hello.java
+********************************************************************************************** Kendra Woolfolk 
+* Janurary 19, 2022
+* CMSC 255 C90 
+********************************************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+	System.out.println("Kendra Woolfolk");
+	System.out.println("Hello.java");
+	System.out.println("Java Lab 01");
+	System.out.println("CMSC 255 C90");
+	System.out.println();
+	}
+}
